c6726d8c1758492397171c9f035d907542774a62
Paula Manese <paulamanese@gmail.com>
2022-11-06 22:20:22 +0100

    again

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -7,10 +7,10 @@ public class AdvancedNamingConvention {
     private int phoneNumber;
 
     void callToFriend() {
-        //some code;
+        //some code
     }
 
-    void callByNumber(int phoneNumber) {
-        //some code;
+    void callByNumber(int number) {
+        //some code
     }
 }
--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,16 +3,11 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable_one = 2000;
+        int variableOne = 2000;
         int value = 1000;
-        int variable_two = 4000;
-        int value_two = 3000;
-        int variable_three = 5000;
-        int variable_four = 7000;
-        int value_three = 90000;
-        int number = 100000;
-        int my_integer = 1000000;
-        int my_var = 1234567;
-        int my__integer = 1234567;
+        int variableTwo = 4000;
+        int value2 = 3000;
+        int variables = 5000;
+        variables = 7000;
+        int valueTwo = 90000;
     }
-}
