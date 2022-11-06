bce3626efae58ef8af8304bce791021ed6aaf080
Paula Manese <paulamanese@gmail.com>
2022-11-06 12:31:31 +0100

    add

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -10,5 +10,9 @@ public class CopiedNames {
         int variableFour = 5000;
         int variableFive = 7000;
         int valueFour = 90000;
+        int number = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myTntegerOne = 1234567;
     }
 }
