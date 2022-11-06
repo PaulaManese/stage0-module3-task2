937bcc476c0ebf20dc10485ec450a53759f41c90
Paula Manese <paulamanese@gmail.com>
2022-11-06 11:43:11 +0100

    tried again

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adultAge = 18;
+    private static final int ADULT_AGE = 18;
 
-    private int Age;
+    private int age;
     private int phoneNumber;
 
-    void CallToFriend() {
-        return;
+    void callToFriend() {
+        //some code;
     }
 
-    void callByNumber(int Number) {
-        return;
+    void callByNumber(int phoneNumber) {
+        //some code;
     }
 }
